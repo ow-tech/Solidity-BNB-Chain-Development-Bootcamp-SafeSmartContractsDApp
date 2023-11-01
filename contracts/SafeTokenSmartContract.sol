b26118373a16a4f3ec52936c97558f4cdf16aa8b
AlexMBarasa <alex.barasa@valcom.ae>
2023-11-01 23:31:54 +0400

    made changes to y logic, added security checks of only user, a user can only create a single account

--- contracts/SafeTokenSmartContract.sol
+++ contracts/SafeTokenSmartContract.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract SafeTokenSmartContract {
+
+     using SafeMath for uint256;
 
     // create user. User struct, mapping user address for easy access
 
@@ -13,44 +16,66 @@ contract SafeTokenSmartContract {
         // uint256 rewards;
         uint256 lokedTimestamp;
         uint256 unlockedTimeStamp;
+      
         bool locked;
         
     }
 
     mapping (address => User) public users;
+    mapping (address => bool) public hasAccount;
     address[] public userAddresses;
-  // Internal function to get the current user's storage reference
-    function _getCurrentUser() private view returns (User storage) {
-        return users[msg.sender];
+
+
+    event TokensDeposited(address indexed user, uint256 amount);
+    event TokensUnlocked(address indexed user,bool);
+    // event AccruedRewards(address indexed user, uint256 amount);
+
+       address private owner;
+
+    constructor() {
+        owner = msg.sender;
     }
 
+   modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
-    function createNewUser(string memory _name) public{
-        User memory newUser =User(_name, payable(msg.sender),0 ether, 0 ,0, false);
+ 
+
+
+    function createNewUser(string memory _name) external {
+        // check if user already exists
+        require(!hasAccount[msg.sender], "User already has an account");
+
+        User memory newUser =User(_name, payable(msg.sender),0 wei, 0 ,0,false);
         users[msg.sender] = newUser;
         userAddresses.push(msg.sender);
+          hasAccount[msg.sender] = true;
       
-
     }
 
-    // all tokens will remain locked for a period of 1 year to attain rewards. if unlocked before
-    //  a year gets completed, they loose their accrued rewards
-    // if 1 year, and user does not unclock tokens, we continue with compounded interest to
-    //  the new princpal.Hence for loked tokens to realise rewards, they must undergo a full circle of 12 months.
+    // all tokens will remain locked for a max period of 1 year. if unlocked before
+    //  a year gets completed, they daily rewards will be calculated
+    // if 1 year, and user does not unclock tokens, will automatically unlock the tokens
 
-    function depositTokensAndLock(uint256 _tokens) public{
-        User storage currentUser = _getCurrentUser();
-        currentUser.tokens = _tokens;
-        currentUser.locked =true;
+    function depositTokensAndLock(uint256 _tokens) external onlyOwner {
+        require(hasAccount[msg.sender], "Create Account to continue");
+        User storage currentUser = users[msg.sender];
+        require( !currentUser.locked, "Unlock tokens");
+        require(_tokens >= 100, "Minimum deposit requirement is 100 tokens");
+         currentUser.tokens += _tokens;
         currentUser.lokedTimestamp=block.timestamp;
+         emit TokensDeposited(msg.sender, _tokens);
 
     }
 
     // lock and unlock tokens
 
-    function lockAndUnlock(bool _lock) public {
-          User storage currentUser = _getCurrentUser();
+    function lockAndUnlock(bool _lock) external onlyOwner {
+        User storage currentUser = users[msg.sender];
           if(_lock && !currentUser.locked){
+    
             currentUser.locked =_lock;
             currentUser.lokedTimestamp=block.timestamp;
             currentUser.unlockedTimeStamp=0;
@@ -58,14 +83,22 @@ contract SafeTokenSmartContract {
           }else if(!_lock && currentUser.locked){
             currentUser.locked = _lock;
             currentUser.unlockedTimeStamp = block.timestamp;
+           
+            // calculate rewards and add them to tokens
+               uint256 rewards = showUserRewards();
+        
+            // Add rewards to tokens
+            currentUser.tokens += rewards;
+             
           }
+        emit TokensUnlocked(msg.sender, currentUser.locked);
 
     }
 
     // calculate Rewards
 
     function showUserRewards() public view returns(uint256){
-        User memory currentUser =_getCurrentUser();
+       User memory currentUser = users[msg.sender];
 
         uint256 currentunlockedTimeStamp;
 
@@ -77,25 +110,24 @@ contract SafeTokenSmartContract {
         
         }
  
-            uint256 secondsInYear = 31556952; // Average number of seconds in a year (365.25 days)
+    uint256 secondsInDay = 86400; // Number of seconds in a day (24 hours * 60 minutes * 60 seconds)
         
-            uint256 numberOfYears = (currentunlockedTimeStamp - currentUser.lokedTimestamp) / secondsInYear;
+            uint256 numberOfDays = (currentunlockedTimeStamp - currentUser.lokedTimestamp) / secondsInDay;
             
        uint256 totalAmount = currentUser.tokens;
-       uint256 rewards;
-        for (uint256 i = 0; i < numberOfYears; i++) {
-            // Calculate interest for the current year
-            uint256 interest = (totalAmount * 2) / 100;
-            // Add the interest to the total amount
-            totalAmount += interest;
-            rewards +=interest;
+      uint256 rewards = 0;
+        for (uint256 i = 0; i < numberOfDays; i++) {
+            // Calculate interest for the current day 1%
+          uint256 interest = totalAmount.mul(1e16).div(1e18); // 1% of totalAmount in wei
+        totalAmount = totalAmount.add(interest);
+        rewards = rewards.add(interest);
         }
-
-        return rewards;
+   
+    // emit AccruedRewards(msg.sender, rewards);
+    return rewards;
 
 
     }
-
 
     function getAllUsers() view public returns(User[] memory){
          uint256 numberOfUsers = userAddresses.length;
